01f114297b7b8e2ff1fc9d2a33a2010a254fd430
jamsheerm <jamsheer@nejoumaljazeera.com>
2022-04-24 09:53:08 +0400

    customer layout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,12 @@
 import '../styles/global.css';
-import { SessionProvider } from "next-auth/react"
 
 import { AppProps } from 'next/app';
+import { SessionProvider } from 'next-auth/react';
 
-const MyApp = ({ Component, pageProps: { session, ...pageProps } }: AppProps) => (
+const MyApp = ({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps) => (
   <SessionProvider session={session} refetchInterval={300 * 60}>
     <Component {...pageProps} />
   </SessionProvider>
