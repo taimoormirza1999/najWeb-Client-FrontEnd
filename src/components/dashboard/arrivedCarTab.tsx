3f51379608de4656b8a47224a4bbb4b2fb719c38
jamsheerm <jamsheer@nejoumaljazeera.com>
2022-06-07 09:55:46 +0400

    update

--- src/components/dashboard/arrivedCarTab.tsx
+++ src/components/dashboard/arrivedCarTab.tsx
@@ -195,7 +195,7 @@ const ArrivedCarTab = ({
                         </Tab.Panels>
                       </Tab.Group>
                       <a
-                        href={`/api/customer/downloadimages/?type=warehouse&car_id=${carId}`}
+                        href={`/api/customer/downloadimages/?type=store&car_id=${carId}`}
                         className="mt-4 inline-flex items-center rounded border border-transparent bg-indigo-600 px-2.5 py-1.5 text-xs font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
                       >
                         Download
