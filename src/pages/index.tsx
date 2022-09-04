caa9885a6415300dc9d2c01d03051f54fb222ad6
jamsheerm <jamsheer@nejoumaljazeera.com>
2022-09-04 10:35:10 +0400

    home page transparent element

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -22,20 +22,20 @@ const Index = ({ announcements }) => {
           alt="banner"
         />
         <div className="absolute top-[10px] left-[10px] max-w-[500px] text-white lg:left-[7%] lg:top-1/3">
-          <h4 className="text-xl font-semibold lg:text-3xl">
+          <h4 className="text-base font-semibold md:text-xl lg:text-3xl">
             Shipping your Cars from <br/>
             all American Auctions
           </h4>
 
-          <p className="mt-[10px] text-base lg:text-xl">
+          <p className="text-sm md:mt-[10px] md:text-base lg:text-xl">
             Working day and night to facilitate your car transportation starting
             from providing buyer accounts, towing, shipping, and tracking it at
             all times
           </p>
         </div>
-        <div className="absolute bottom-0 left-1/2 flex w-[16rem] -translate-x-1/2 -translate-y-1/2 flex-col justify-center rounded-[25px] bg-transparent p-4 md:h-[35vh] md:w-[20rem] lg:bg-[rgb(0,95,183)]/[0.6] lg:left-auto lg:right-[1%] lg:top-[22%] lg:w-[30rem] xl:h-[45vh] xl:w-[35rem] xl:translate-x-0 xl:translate-y-0 2xl:right-[8%] 2xl:h-[65%] 2xl:w-[30%]">
+        <div className="absolute bottom-[-8px] left-1/2 flex w-[16rem] -translate-x-1/2 -translate-y-1/2 flex-col justify-center rounded-[25px] bg-transparent md:top-1/2 md:h-[20vh] md:w-[20rem] md:bg-[rgb(0,95,183)]/[0.6] md:p-4 lg:left-auto lg:right-[1%] lg:top-[22%] lg:w-[30rem] xl:h-[45vh] xl:w-[35rem] xl:translate-x-0 xl:translate-y-0 2xl:right-[8%] 2xl:h-[65%] 2xl:w-[30%]">
           <div className="basis-1/3">
-            <h2 className="hidden py-6 text-center text-2xl font-semibold text-white md:text-3xl lg:block lg:text-5xl rtl:lg:text-5xl xl:text-[70px]">
+            <h2 className="hidden py-6 text-center text-2xl font-semibold text-white md:block md:text-3xl lg:text-5xl rtl:lg:text-5xl xl:text-[70px]">
               <FormattedMessage id="you.want.to" />
             </h2>
 
