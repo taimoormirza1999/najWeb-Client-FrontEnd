512adf436c1a07ccfc8dce067e0838a989e79d01
Wajid <wajid@nejoumaljazeera.com>
2023-05-23 14:11:55 +0400

    container no in statement for bulk customer

--- src/components/userContext.tsx
+++ src/components/userContext.tsx
@@ -1,5 +1,35 @@
-import { createContext } from 'react';
+import { useRouter } from 'next/router';
+import { useSession } from 'next-auth/react';
+import { createContext, useEffect, useState } from 'react';
 
-const UserContext = createContext();
+export const UserContext = createContext({});
 
-export default UserContext;
+export function UserContextProvider({ children }) {
+  const [profile, setProfile] = useState({});
+  const { data: session } = useSession();
+  const router = useRouter();
+  const { locale } = router;
+
+  useEffect(() => {
+    const isBulkShippingCustomer = session?.profile[0]?.bulk_shipLoad === '1';
+
+    const allowWarehouseCarsRequests =
+      session?.profile[0]?.allowWarehouseCarsRequests === '1';
+
+    const fullName =
+      locale === 'ar'
+        ? session?.profile[0]?.full_name_ar
+        : session?.profile[0]?.full_name;
+
+    setProfile({
+      ...session?.profile[0],
+      fullName,
+      isBulkShippingCustomer,
+      allowWarehouseCarsRequests,
+    });
+  }, [session?.profile]);
+
+  return (
+    <UserContext.Provider value={{ profile }}>{children}</UserContext.Provider>
+  );
+}
