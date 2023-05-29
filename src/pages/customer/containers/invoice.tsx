93fc51876bac12a9e93a730092bfc6882238a5c3
kasim-nejoum <mahammad.kasim@naj.ae>
2023-05-29 17:04:17 +0400

    Changes in container invoice

--- src/pages/customer/containers/invoice.tsx
+++ src/pages/customer/containers/invoice.tsx
@@ -36,17 +36,23 @@ const ContainerInvoice = ({ invoice }) => {
 
       <div className="grid grid-cols-3 p-5">
           <div className='mt-[12px]'>
-            <Image src={`/assets/images/logo-en.png`} width="450px" height="120px" alt="" />
+            <Image src={`/assets/images/Full_Logo.png`} width="550px" height="120px" alt="" />
           </div>
           <div className='mt-[11px]'>
-            <Image src={`/assets/images/logo-ar.png`} width="450px" height="120px" alt="" />
+            {/* <Image src={`/assets/images/logo-ar.png`} width="450px" height="120px" alt="" /> */}
           </div>
-          <div className='mt-[15px] print:mt-[5px] ml-[20px] text-[13px]'>
-            <p className='font-bold'>Nejoum Aljazeera Used Cars L.L.C.</p>
-            <p>Industiral Area 4, Head Office</p>
-            <p>Sharjah, UAE</p>
-            <p>info@naj.ae | +971 65 440 202</p>
-            <p>www.naj.ae </p>
+
+          <div className='mt-[15px] print:mt-[5px] ml-[20px] text-[12px] text-dark-blue'>
+            <span className='float-left'>+971 6 544 0202</span>
+            <span className='float-right'>/NejoumAljazeera</span> <br/>
+            <span className='float-left'>info@naj.ae</span>
+            <span className='float-right'>www.naj.ae</span> <br/>
+            <div>
+              <span className='font-bold'>NEJOUM</span>
+              <span> ALJAZEERA GROUP</span>
+            </div>
+            <p>P.OBOX. : 37027 - Industrial Area 4,</p>
+            <p>Sharjah, United Arab Emirates</p>
           </div>
       </div>
       <Head>
@@ -54,40 +60,50 @@ const ContainerInvoice = ({ invoice }) => {
       </Head>
       <div className="p-5 mr-3px">
 
-        <div className="grid grid-cols-4 gap-4 font-bold pl-2 pr-2 -mt-[25px] print:-mt-[45px]">
-          <div className='col-span-2'>
-            <p className='font-bold text-[12px]'>Bank Details</p>
+        <div className="grid grid-cols-4 gap-4 font-bold pl-2 pr-2 -mt-[20px] print:-mt-[32px]">
+          <div>
+              <p className='font-bold text-[12px]'>Bank Details</p>
+          </div>
+          <div>
+            <p className='font-bold text-[12px] float-right'>التفاصيل المصرفية</p>
+          </div>
+          <div>
+            <p className='font-bold text-[12px]'>Invoice</p>
+          </div>
+          <div>
+            <p className='font-bold text-[12px] float-right mr-[5px]'>فاتورة</p>
+          </div>
+
+          <div className='col-span-2 -mt-[15px]'>
             <div className="border-dark-blue border-[1px] border-solid w-[100%] ">
               </div>
           </div>
-          <div className='col-span-2'>
-            <p className='font-bold text-[12px]'>Invoice</p>
+          <div className='col-span-2 -mt-[15px]'>
                 <div className="border-dark-blue border-[1px] border-solid w-[100%] ">
                   </div>
           </div>
-       
-            <div>
+            <div className='-mt-[22px]'>
               <p>Bank Name : </p>
               <p>Bank Address : </p>
               <p>IBAN : </p>
               <p>Account Number : </p>
               <p>Swift code for international wires  : </p>
             </div>
-            <div>
+            <div className='-mt-[22px]'>
               <p>{accountName}</p>
                 <p>Sharjah, UAE</p>
                 <p>{iban}</p>
                 <p>{accountNumber}</p>
                 <p></p>
             </div>
-            <div>
+            <div className='-mt-[22px]'>
               <p>Number :</p>
               <p>Creation Date : </p>
               <p>Due Date : </p>
               <p>Post Due Days : </p>
               <p>Invoice printed : </p>
             </div>
-            <div>
+            <div className='-mt-[22px]'>
               <p>1111111</p>
               <p>{new Date().toLocaleDateString()}</p>
               <p>2023-11-11</p>
@@ -174,10 +190,10 @@ const ContainerInvoice = ({ invoice }) => {
                 <h5>Nejoum Al Jazeera</h5>
               </td>
               <td>
-                <h5>Booking Num/رقم الحجز</h5>
+                <h5>Consignee/ المرسل إليه</h5>
               </td>
               <td>
-                <h5>{bookingNumber}</h5>
+                <h5>Nejoum Al Jazeera</h5>
               </td>
             </tr>
             <tr>
@@ -218,50 +234,50 @@ const ContainerInvoice = ({ invoice }) => {
               <h5>{etd}</h5>
               </td>
               <td>
-                <h5></h5>
+                <h5>Booking Num/رقم الحجز</h5>
               </td>
               <td>
-                <h5></h5>
+                <h5>{bookingNumber}</h5>
               </td>
             </tr>
           </tbody>
         </table>
 
         {cars && cars.length > 1 ? (
-          <table className="my-3 w-full font-bold mt-[40px]">
-            <thead className="text-dark-blue border-dark-silver  mt-1 rounded-xl border-[1px] p-2">
+          <table className="my-3 w-full font-bold mt-[40px] '">
+            <thead className="text-dark-blue border-[#c0c0c0] mt-1 rounded-xl border-[1px] p-2 ">
               <tr>
                 <th>وصف</th>
-                <th className='border-l-[1px] border-dark-silver'> المزاد</th>
-                <th className='border-l-[1px] border-dark-silver'>سعر</th>
-                <th className='border-l-[1px] border-dark-silver'> النقل الداخلي</th>
-                <th className='border-l-[1px] border-dark-silver'>شحن</th>
-                <th className='border-l-[1px] border-dark-silver'>تخليص</th>
-                <th className='border-l-[1px] border-dark-silver'>التأخر في السداد</th>
-                <th className='border-l-[1px] border-dark-silver'>رافعة شوكية</th>
-                <th className='border-l-[1px] border-dark-silver'>تخزين المزاد</th>
-                <th className='border-l-[1px] border-dark-silver'>أخرى</th>
-                <th className='border-l-[1px] border-dark-silver' >المجموع</th>
+                <th className='border-l-[1px] border-[#c0c0c0]'> المزاد</th>
+                <th className='border-l-[1px] border-[#c0c0c0]'>سعر</th>
+                <th className='border-l-[1px] border-[#c0c0c0]'> النقل الداخلي</th>
+                <th className='border-l-[1px] border-[#c0c0c0]'>شحن</th>
+                <th className='border-l-[1px] border-[#c0c0c0]'>تخليص</th>
+                <th className='border-l-[1px] border-[#c0c0c0]'>التأخر في السداد</th>
+                <th className='border-l-[1px] border-[#c0c0c0]'>رافعة شوكية</th>
+                <th className='border-l-[1px] border-[#c0c0c0]'>تخزين المزاد</th>
+                <th className='border-l-[1px] border-[#c0c0c0]'>أخرى</th>
+                <th className='border-l-[1px] border-[#c0c0c0]'>المجموع</th>
               </tr>
               <tr>
                 <th>Description</th>
-                <th className='border-l-[1px] border-dark-silver'>Auction</th>
-                <th className='border-l-[1px] border-dark-silver'>Price</th>
-                <th className='border-l-[1px] border-dark-silver'>Towing</th>
-                <th className='border-l-[1px] border-dark-silver'>Shipping</th>
-                <th className='border-l-[1px] border-dark-silver'>Clearance</th>
-                <th className='border-l-[1px] border-dark-silver'>Late Payment</th>
-                <th className='border-l-[1px] border-dark-silver'>Forklift</th>
-                <th className='border-l-[1px] border-dark-silver'>Auction Storage</th>
-                <th className='border-l-[1px] border-dark-silver'>Other</th>
-                <th className='border-l-[1px] border-dark-silver'>Total</th>
+                <th className='border-l-[1px] border-[#c0c0c0]'>Auction</th>
+                <th className='border-l-[1px] border-[#c0c0c0]'>Price</th>
+                <th className='border-l-[1px] border-[#c0c0c0]'>Towing</th>
+                <th className='border-l-[1px] border-[#c0c0c0]'>Shipping</th>
+                <th className='border-l-[1px] border-[#c0c0c0]'>Clearance</th>
+                <th className='border-l-[1px] border-[#c0c0c0]'>Late Payment</th>
+                <th className='border-l-[1px] border-[#c0c0c0]'>Forklift</th>
+                <th className='border-l-[1px] border-[#c0c0c0]'>Auction Storage</th>
+                <th className='border-l-[1px] border-[#c0c0c0]'>Other</th>
+                <th className='border-l-[1px] border-[#c0c0c0]'>Total</th>
               </tr>
             </thead>
 
             <tbody className="text-center">
               {cars.map((car, i) => (
                 <tr key={i}>
-                  <td className="border-dark-silver border-b-[1px] border-l-[1px]  border-r-[1px] text-left">
+                  <td className="border-[#c0c0c0] border-b-[1px] border-l-[1px]  border-r-[1px] text-left">
                     <div className="mt-1.5 pl-[1px]">
                       {`${car.carMakerName} ${car.carModelName} ${car.year}`}{' '}
                       <br />
@@ -269,34 +285,34 @@ const ContainerInvoice = ({ invoice }) => {
                       VIN: {car.vin} <br />
                     </div>
                   </td>
-                  <td className="border-dark-silver border-r-[1px] border-b-[1px] ">
+                  <td className="border-[#c0c0c0] border-r-[1px] border-b-[1px] ">
                     {car.auction_title}
                   </td>
-                  <td className="border-dark-silver border-b-[1px] border-r-[1px]">
+                  <td className="border-[#c0c0c0] border-b-[1px] border-r-[1px]">
                     ${car.car_cost}
                   </td>
-                  <td className="border-dark-silver border-b-[1px] border-r-[1px]">
+                  <td className="border-[#c0c0c0] border-b-[1px] border-r-[1px]">
                       {car?.shippingAmount || 0}
                   </td>
-                  <td className="border-dark-silver border-b-[1px] border-r-[1px]">
+                  <td className="border-[#c0c0c0] border-b-[1px] border-r-[1px]">
                      {car?.clearanceAmount || 0}
                   </td>
-                  <td className="border-dark-silver border-b-[1px] border-r-[1px]">
+                  <td className="border-[#c0c0c0] border-b-[1px] border-r-[1px]">
                      {car?.towingAmount || 0}
                   </td>
-                  <td className="border-dark-silver border-b-[1px] border-r-[1px]">
+                  <td className="border-[#c0c0c0] border-b-[1px] border-r-[1px]">
                       {car?.shippingAmount || 0}
                   </td>
-                  <td className="border-dark-silver border-b-[1px] border-r-[1px]">
+                  <td className="border-[#c0c0c0] border-b-[1px] border-r-[1px]">
                      {car?.clearanceAmount || 0}
                   </td>
-                  <td className="border-dark-silver border-b-[1px] border-r-[1px]">
+                  <td className="border-[#c0c0c0] border-b-[1px] border-r-[1px]">
                      {car?.towingAmount || 0}
                   </td>
-                  <td className="border-dark-silver border-b-[1px] border-r-[1px]">
+                  <td className="border-[#c0c0c0] border-b-[1px] border-r-[1px]">
                      {car?.otherAmount || 0}
                   </td>
-                  <td className="border-dark-silver border-b-[1px] border-r-[1px]"> {car?.totalAmount || 0}</td>
+                  <td className="border-[#c0c0c0] border-b-[1px] border-r-[1px]"> {car?.totalAmount || 0}</td>
                 </tr>
               ))}
 
@@ -311,10 +327,10 @@ const ContainerInvoice = ({ invoice }) => {
                 <td></td>
                 <td></td>
                 <td></td>
-                  <td className="border-dark-silver border-l-[1px] border-b-[1px] border-r-[1px] text-left">
+                  <td className="border-[#c0c0c0] border-l-[1px] border-b-[1px] border-r-[1px] text-left">
                     Total/إجمالي
                   </td>
-                  <td className="border-dark-silver border-b-[1px] border-r-[1px] ">
+                  <td className="border-[#c0c0c0] border-b-[1px] border-r-[1px] ">
                     {totalAmount.toFixed(2)}
                   </td>
                  </tr>
@@ -328,10 +344,10 @@ const ContainerInvoice = ({ invoice }) => {
                 <td></td>
                 <td></td>
                 <td></td>
-                  <td className="border-dark-silver border-l-[1px] border-b-[1px] border-r-[1px] text-left">
+                  <td className="border-[#c0c0c0] border-l-[1px] border-b-[1px] border-r-[1px] text-left">
                     Payment/دفع
                   </td>
-                  <td className="border-dark-silver border-b-[1px] border-r-[1px] ">
+                  <td className="border-[#c0c0c0] border-b-[1px] border-r-[1px] ">
                     {totalAmount.toFixed(2)}
                   </td>
                  </tr>
@@ -345,10 +361,10 @@ const ContainerInvoice = ({ invoice }) => {
                 <td></td>
                 <td></td>
                 <td></td>
-                  <td className="border-dark-silver border-l-[1px] border-b-[1px] border-r-[1px] text-left">
+                  <td className="border-[#c0c0c0] border-l-[1px] border-b-[1px] border-r-[1px] text-left">
                     Balance Due/الرصيد المستحق
                   </td>
-                  <td className="border-dark-silver border-b-[1px] border-r-[1px] ">
+                  <td className="border-[#c0c0c0] border-b-[1px] border-r-[1px] ">
                     {balance}
                   </td>
                  </tr>
@@ -359,34 +375,26 @@ const ContainerInvoice = ({ invoice }) => {
           </table>
         ) : null}
 
-
-      <div className="grid justify-items-center  text-dark-blue mt-10 print:fixed print:bottom-[25px] print:left-[18%]">
-          {/* <h3 className="text-lg font-bold">Terms & Condition</h3> */}
-
-                <div >
-                  <p className='text-[14px]'>
-                  يرجى العلم بأنه تم تعديل هذا الفاتورة في {new Date().toLocaleDateString()} {' '}
-                    
-                  </p> 
-                </div>
+          <div className="grid grid-cols-2 text-dark-blue print:bottom-[50px] print:fixed mr-[40px]">
                 <div>
-                  <p className=" font-bold text-[15px]">
+                    <p className='tracking-wide text-[10px]'>
+                        PLEASE BE ADVISED THAT THIS INVOICE HAS BEEN AMENDED ON {' '}
+                        {new Date().toLocaleDateString()}
+                      </p> 
+                      <p className=" font-bold tracking-wide text-[10px]">
+                        CASH, MONEY ORDERS, OR THIRD-PARTY PAYMENTS WILL NOT BE ACCEPTED.
+                  </p>
+                </div>
+                <div className='text-right'>
+                  <p className='text-[14px]'>
+                    يرجى العلم بأنه تم تعديل هذا الفاتورة في {new Date().toLocaleDateString()} {' '}
+                    </p> 
+       
+                    <p className="font-bold text-[14px]">
                   لن يتم قبول النقد أو الأوامر المالية أو مدفوعات الطرف 
 الثالث
                   </p>
                 </div>
-                <div className='mt-5'>
-                  <p className='tracking-wide text-[14px]'>
-                    PLEASE BE ADVISED THAT THIS INVOICE HAS BEEN AMENDED ON {' '}
-                    {new Date().toLocaleDateString()}
-                  </p> 
-                </div>
-                <div>
-                  <p className=" font-bold tracking-wide text-[15px]">
-                    CASH, MONEY ORDERS, OR THIRD-PARTY PAYMENTS WILL NOT BE ACCEPTED.
-                  </p>
-                </div>
-            
           </div>
           </div>
     </section>
