5634733d9044353ad8c5775a89397cfa843f1f4c
Nejoum-aljazeera <71590149+Nejoum-aljazeera@users.noreply.github.com>
2023-10-18 16:40:18 +0000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,46 +1,39 @@
-/* eslint-disable import/no-extraneous-dependencies */
 const path = require('path')
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true",
 });
 
-// const { i18n } = require("./next-i18next.config");
+const { i18n } = require("./next-i18next.config");
 
-// module.exports = withBundleAnalyzer({
-//   eslint: {
-//     ignoreDuringBuilds: true,
-//     dirs: ["."],
-//   },
-//   typescript: {
-//     ignoreBuildErrors: true,
-//   },
-//   poweredByHeader: false,
-//   trailingSlash: true,
-//   basePath: "",
-//   // The starter code load resources from `public` folder with `router.basePath` in React components.
-//   // So, the source code is "basePath-ready".
-//   // You can remove `basePath` if you don't need it.
-//   reactStrictMode: false,
-//   i18n: {
-//     defaultLocale: "en",
-//     locales: ["ar", "en"],
-//     localePath: path.resolve('./public/locales'),
-//   },
-//   images: {
-//     domains: ["cdn.nejoumaljazeera.co", "nejoumaljazeera.co"],
-//   },
-// });
-
-
-
-module.exports = {
+module.exports = withBundleAnalyzer({
+  eslint: {
+    ignoreDuringBuilds: true,
+    dirs: ["."],
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  poweredByHeader: false,
+  trailingSlash: true,
+  basePath: "",
+  reactStrictMode: false,
+  i18n: {
+    defaultLocale: "en",
+    locales: ["ar", "en"],
+    localePath: path.resolve('./public/locales'),
+  },
+  images: {
+    domains: ["cdn.nejoumaljazeera.co", "nejoumaljazeera.co"],
+  },
+  
+  // Adding the redirect configuration here:
   async redirects() {
     return [
       {
         source: '/:path*',
         destination: 'https://new.naj.ae/:path*',
-        permanent: true, 
+        permanent: true, // Change to false if you want a 302 redirection instead of 301
       },
     ]
   },
-}
+});
