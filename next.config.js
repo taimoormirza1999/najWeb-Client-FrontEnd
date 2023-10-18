fcc5efb458568a0a617e6fab363bc43b6706aba4
Nejoum-aljazeera <71590149+Nejoum-aljazeera@users.noreply.github.com>
2023-10-18 16:38:09 +0000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,29 +4,43 @@ const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true",
 });
 
-const { i18n } = require("./next-i18next.config");
+// const { i18n } = require("./next-i18next.config");
 
-module.exports = withBundleAnalyzer({
-  eslint: {
-    ignoreDuringBuilds: true,
-    dirs: ["."],
+// module.exports = withBundleAnalyzer({
+//   eslint: {
+//     ignoreDuringBuilds: true,
+//     dirs: ["."],
+//   },
+//   typescript: {
+//     ignoreBuildErrors: true,
+//   },
+//   poweredByHeader: false,
+//   trailingSlash: true,
+//   basePath: "",
+//   // The starter code load resources from `public` folder with `router.basePath` in React components.
+//   // So, the source code is "basePath-ready".
+//   // You can remove `basePath` if you don't need it.
+//   reactStrictMode: false,
+//   i18n: {
+//     defaultLocale: "en",
+//     locales: ["ar", "en"],
+//     localePath: path.resolve('./public/locales'),
+//   },
+//   images: {
+//     domains: ["cdn.nejoumaljazeera.co", "nejoumaljazeera.co"],
+//   },
+// });
+
+
+
+module.exports = {
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        destination: 'https://new.naj.ae/:path*',
+        permanent: true, 
+      },
+    ]
   },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  poweredByHeader: false,
-  trailingSlash: true,
-  basePath: "",
-  // The starter code load resources from `public` folder with `router.basePath` in React components.
-  // So, the source code is "basePath-ready".
-  // You can remove `basePath` if you don't need it.
-  reactStrictMode: false,
-  i18n: {
-    defaultLocale: "en",
-    locales: ["ar", "en"],
-    localePath: path.resolve('./public/locales'),
-  },
-  images: {
-    domains: ["cdn.nejoumaljazeera.co", "nejoumaljazeera.co"],
-  },
-});
+}
