332d58c1d514a9e82613143b88f98894d912b8ef
jamsheerm <jamsheer@nejoumaljazeera.com>
2023-11-14 17:54:22 +0400

    arrived to port

--- src/components/dashboard/shippingCarTab.tsx
+++ src/components/dashboard/shippingCarTab.tsx
@@ -1,103 +1,27 @@
 import { faFilePdf } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Dialog } from '@headlessui/react';
 import { CheckCircleIcon } from '@heroicons/react/outline';
 import { XCircleIcon } from '@heroicons/react/solid';
 import Link from 'next/link';
-import React from 'react';
-import { FormattedMessage } from 'react-intl';
+import { useSession } from 'next-auth/react';
+import { useEffect, useRef, useState } from 'react';
+import { FormattedMessage, useIntl } from 'react-intl';
 
 import {
   Pagination,
   SelectPageRecords,
 } from '@/components/dashboard/pagination';
 import { classNames } from '@/utils/Functions';
+import { postData } from '@/utils/network';
 
 import ImagesViewer from '../cars/ImagesViewer';
 import SingleImagesViewer from '../common/SingleImagesViewer';
+import CustomModal from '../customModal';
 import NotesButtonModal from '../NotesButtonModal';
 import TableColumn from '../TableColumn';
 import TableHeader from '../TableHeader';
 import TableHeadText from '../TableHeadText';
-
-const carTableHeader = [
-  { name: 'page.customer.dashboard.table.no' },
-  {
-    name: 'page.customer.dashboard.table.auction_photo',
-  },
-  {
-    name: 'page.customer.dashboard.table.detail',
-  },
-  {
-    name: 'page.customer.dashboard.table.lot_vin',
-  },
-  {
-    name: 'page.customer.dashboard.table.auction',
-  },
-  {
-    header: 'page.customer.dashboard.table.buyer_number',
-  },
-  {
-    header: 'page.customer.dashboard.table.region',
-  },
-  {
-    name: 'page.customer.dashboard.table.destination',
-  },
-  {
-    name: 'page.customer.dashboard.table.purchase_date',
-  },
-  {
-    name: 'page.customer.dashboard.table.payment_date',
-  },
-  {
-    name: 'page.customer.dashboard.table.date_pick',
-  },
-  {
-    name: 'picked_car_title_note',
-  },
-  {
-    name: 'page.customer.dashboard.table.arrived',
-  },
-  {
-    name: 'page.customer.dashboard.table.title',
-  },
-  {
-    header: 'page.customer.dashboard.table.title_note',
-  },
-  {
-    header: 'page.customer.dashboard.table.title_date',
-    order: 'title_date',
-  },
-  {
-    name: 'page.customer.dashboard.table.key',
-  },
-  {
-    name: 'page.customer.dashboard.table.loaded_date',
-  },
-  {
-    name: 'page.customer.dashboard.table.loaded_image',
-  },
-  {
-    name: 'page.customer.dashboard.table.booking',
-  },
-  {
-    name: 'page.customer.dashboard.table.container',
-  },
-  {
-    name: 'page.customer.dashboard.table.etd',
-  },
-  {
-    name: 'page.customer.dashboard.table.shipping_date',
-  },
-  {
-    name: 'page.customer.dashboard.table.shipping_image',
-  },
-  {
-    name: 'page.customer.dashboard.table.eta',
-  },
-  {
-    name: 'dock_receipt',
-  },
-];
 
 const ShippingCarTab = ({
   carsRecords,
@@ -106,11 +30,231 @@ const ShippingCarTab = ({
   limit,
   search = '',
 }) => {
+  const { data: session } = useSession();
+  const intl = useIntl();
+  const [carsArray, setCarsArray] = useState(carsRecords);
   const paginationUrl = `/customer/dashboard?tab=tabs-shipping&search=${search}&limit=${limit}`;
   const limitUrl = `/customer/dashboard?tab=tabs-shipping&page=`;
+  const [inputValue, setInputValue] = useState({
+    message: '',
+  });
   const addIndex = parseInt(limit, 10) && page ? page * limit : 0;
+  const [arrivedPortModalOpen, setArrivedPortModalOpen] = useState(false);
+  const [arrivedPortModalSuccess, setArrivedPortModalSuccess] = useState(false);
+  const [arrivedPortModalError, setArrivedPortModalError] = useState(false);
+  const arrivedPortCancelButtonRef = useRef(null);
+  const selectedCar = useRef(0);
+  useEffect(() => {
+    setCarsArray(carsRecords);
+  }, [carsRecords]);
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const formData = {
+      car_id: selectedCar.current,
+      message: event.target.message.value,
+    };
+
+    const apiURL = '/api/cars/arrivedToPort';
+
+    const response = await postData(apiURL, formData);
+
+    if (response.success === true) {
+      setArrivedPortModalSuccess(true);
+      setArrivedPortModalError(false);
+
+      setCarsArray(
+        carsArray.filter((row) => {
+          return row.car_id !== selectedCar.current;
+        })
+      );
+
+      setInputValue(() => ({
+        message: '',
+      }));
+      selectedCar.current = 0;
+    } else if (arrivedPortModalOpen) {
+      setArrivedPortModalError(true);
+      setArrivedPortModalSuccess(false);
+    }
+  };
+
+  const carTableHeader = [
+    { name: 'page.customer.dashboard.table.no' },
+    {
+      name: 'page.customer.dashboard.table.auction_photo',
+    },
+    {
+      name: 'page.customer.dashboard.table.detail',
+    },
+    {
+      name: 'page.customer.dashboard.table.lot_vin',
+    },
+    {
+      name: 'page.customer.dashboard.table.auction',
+    },
+    {
+      header: 'page.customer.dashboard.table.buyer_number',
+    },
+    {
+      header: 'page.customer.dashboard.table.region',
+    },
+    {
+      name: 'page.customer.dashboard.table.destination',
+    },
+    {
+      name: 'page.customer.dashboard.table.purchase_date',
+    },
+    {
+      name: 'page.customer.dashboard.table.payment_date',
+    },
+    {
+      name: 'page.customer.dashboard.table.date_pick',
+    },
+    {
+      name: 'picked_car_title_note',
+    },
+    {
+      name: 'page.customer.dashboard.table.arrived',
+    },
+    {
+      name: 'page.customer.dashboard.table.title',
+    },
+    {
+      header: 'page.customer.dashboard.table.title_note',
+    },
+    {
+      header: 'page.customer.dashboard.table.title_date',
+      order: 'title_date',
+    },
+    {
+      name: 'page.customer.dashboard.table.key',
+    },
+    {
+      name: 'page.customer.dashboard.table.loaded_date',
+    },
+    {
+      name: 'page.customer.dashboard.table.loaded_image',
+    },
+    {
+      name: 'page.customer.dashboard.table.booking',
+    },
+    {
+      name: 'page.customer.dashboard.table.container',
+    },
+    {
+      name: 'page.customer.dashboard.table.etd',
+    },
+    {
+      name: 'page.customer.dashboard.table.shipping_date',
+    },
+    {
+      name: 'page.customer.dashboard.table.shipping_image',
+    },
+    {
+      name: 'page.customer.dashboard.table.eta',
+    },
+    {
+      name: 'dock_receipt',
+    },
+  ];
+  if (session?.profile[0]?.naj_branch === '1') {
+    carTableHeader.push({
+      name: 'page.customer.dashboard.table.arrived_to_port',
+    });
+  }
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setInputValue((prevState) => ({ ...prevState, [name]: value }));
+  }
+
   return (
     <div className="" id="tabs-shipping" role="tabpanel">
+      <CustomModal
+        showOn={arrivedPortModalOpen}
+        initialFocus={arrivedPortCancelButtonRef}
+        onClose={() => {
+          setArrivedPortModalOpen(false);
+        }}
+      >
+        <div className="text-dark-blue">
+          <Dialog.Title
+            as="h6"
+            className="mb-8 text-xl font-bold leading-6 md:text-xl lg:text-2xl"
+          >
+            <FormattedMessage id="page.customer.dashboard.table.arrived_to_port" />
+          </Dialog.Title>
+          <div className="mt-4">
+            <form method="post" onSubmit={handleSubmit} className="mt-8 mb-4">
+              {arrivedPortModalSuccess === true ? (
+                <div className="text-center">
+                  <i className="material-icons text-yellow-orange mb-4 text-6xl">
+                    &#xe2e6;
+                  </i>
+                  <div className="mt-2">
+                    <p className="mb-4 py-6 text-xl">
+                      <FormattedMessage id="messages.updated_successfully" />
+                    </p>
+                  </div>
+                </div>
+              ) : null}
+
+              {arrivedPortModalError === true ? (
+                <div className="text-center">
+                  <i className="material-icons mb-4 text-6xl text-red-800">
+                    &#xe160;
+                  </i>
+                  <div className="mt-2">
+                    <p className="mb-4 py-6 text-xl">
+                      <FormattedMessage id="general.technicalErr" />
+                    </p>
+                  </div>
+                </div>
+              ) : null}
+
+              {arrivedPortModalSuccess === false &&
+              arrivedPortModalError === false ? (
+                <div>
+                  <div className="relative mt-1 ltr:pl-6 rtl:pr-6">
+                    <textarea
+                      rows={3}
+                      required={true}
+                      className="text-outer-space border-medium-grey w-full resize-none rounded border text-lg focus:border-blue-800 focus:ring-0 ltr:placeholder:italic"
+                      name="message"
+                      placeholder={intl.formatMessage({
+                        id: 'messages.message',
+                      })}
+                      value={inputValue.message}
+                      onChange={handleChange}
+                    ></textarea>
+                    <span className="text-yellow-orange absolute top-0 text-xl font-bold ltr:left-0 rtl:right-0">
+                      *
+                    </span>
+                  </div>
+                  <button
+                    type="submit"
+                    className="border-azure-blue bg-azure-blue hover:bg-dark-blue mx-auto my-6 flex justify-center rounded border-2 py-2 px-8 text-xl font-semibold text-white shadow-sm"
+                  >
+                    {<FormattedMessage id="general.submit" />}
+                  </button>
+                </div>
+              ) : null}
+            </form>
+          </div>
+        </div>
+        <div className="mt-8 flex justify-center gap-4 sm:mt-6">
+          <button
+            type="button"
+            className="border-azure-blue text-azure-blue my-4 inline-block max-w-max rounded-md border-2 px-2 py-1  text-sm md:px-4 md:py-2 lg:text-lg"
+            onClick={() => {
+              setArrivedPortModalOpen(false);
+            }}
+            ref={arrivedPortCancelButtonRef}
+          >
+            <FormattedMessage id="general.close" />
+          </button>
+        </div>
+      </CustomModal>
       <div>
         <TableHeadText id={'page.customer.dashboard.allcars'} />
         <div className="flex flex-col">
@@ -122,7 +266,7 @@ const ShippingCarTab = ({
                 <table className="all_tables min-w-full divide-y divide-gray-300">
                   <TableHeader tableHeader={carTableHeader} />
                   <tbody>
-                    {carsRecords.map((car, index) => (
+                    {carsArray.map((car, index) => (
                       <tr
                         key={index}
                         className={classNames(
@@ -292,6 +436,25 @@ const ShippingCarTab = ({
                             <span>NA</span>
                           )}
                         </TableColumn>
+                        {session?.profile[0]?.naj_branch === '1' ? (
+                          <TableColumn scope="col" className="min-w-[47px]">
+                            {car.isUAEPort === '0' ? (
+                              <button
+                                type="button"
+                                className="border-azure-blue text-azure-blue inline-block max-w-max rounded-md border-2 px-2 py-1  text-sm"
+                                onClick={() => {
+                                  setArrivedPortModalSuccess(false);
+                                  setArrivedPortModalError(false);
+                                  setArrivedPortModalOpen(true);
+                                  selectedCar.current = car.car_id;
+                                }}
+                              >
+                                <CheckCircleIcon className="h-4 w-4 text-green-400" />
+                                <FormattedMessage id="page.customer.dashboard.table.arrive" />
+                              </button>
+                            ) : null}
+                          </TableColumn>
+                        ) : null}
                       </tr>
                     ))}
                   </tbody>
