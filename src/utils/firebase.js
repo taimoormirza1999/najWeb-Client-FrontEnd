9cd52345b2df0cdab46fb3162cddc6abb607c4d2
taimoornaj <taimoor@naj.ae>
2024-05-22 12:10:12 +0400

    Fixed Firebase error handling and initialization and SDK if browser doesn't support and upgrading the firbase package

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -4,6 +4,7 @@ import { getMessaging, getToken, onMessage } from "firebase/messaging";
 import localforage from "localforage";
 
 let messaging;
+let firebaseApp;
 const firebaseConfig = {
   apiKey: "AIzaSyAX18nUsueZ8Enmnnr4BIRh9_cq6A_RHVI",
   authDomain: "nejoum-aljazeera-2e42e.firebaseapp.com",
@@ -14,9 +15,24 @@ const firebaseConfig = {
   appId: "1:838558519710:web:0baa5b8bfe159b7271c68f",
   measurementId: "G-PZRT1EY2M1",
 };
-const firebaseApp = initializeApp(firebaseConfig);
-
+// Check if the code is running in the client-side environment
+if (typeof window !== "undefined") {
+  // Initialize Firebase app
+   firebaseApp = initializeApp(firebaseConfig);
+// Check if the browser supports Firebase messaging
+if ('Notification' in window && 'serviceWorker' in navigator && 'PushManager' in window) {
+  messaging = getMessaging(firebaseApp);
+} else {
+  // Handle unsupported browser error
+  console.warn('This browser does not support Firebase messaging');
+  // You can display a message to the user or take other actions as needed
+}}
 const findToken = () => {
+  // firebaseApp is initialized before calling findToken
+  if (!firebaseApp) {
+    console.error("Firebase app is not initialized");
+    return;
+  }
   messaging = getMessaging(firebaseApp);
   return getToken(messaging, {
     vapidKey:
