0f8b385f4e2b3299b82bdc2538a1ba6a45799723
Ibrahem Mahyob <ibrahemid@gmail.com>
2024-05-25 00:22:47 +0700

    saved Sat May 25 00:22:47 WIB 2024 modified files count: 1 src/components/cars/TowingCarsRequestForm.tsx added files count: 0
    deleted files count: 0

--- src/components/cars/TowingCarsRequestForm.tsx
+++ src/components/cars/TowingCarsRequestForm.tsx
@@ -3,6 +3,7 @@ import 'react-form-wizard-component/dist/style.css';
 import { Dialog } from '@headlessui/react';
 import { InformationCircleIcon, XCircleIcon } from '@heroicons/react/outline';
 import axios from 'axios';
+import { useRouter } from 'next/router';
 import { useContext, useEffect, useRef, useState } from 'react';
 import FormWizard from 'react-form-wizard-component';
 import { FormattedMessage, useIntl } from 'react-intl';
@@ -15,7 +16,7 @@ import { classNames, sentenceCase } from '@/utils/Functions';
 import CustomModal from '../customModal';
 import { CarIcon, SpinnerIcon } from '../themeIcons';
 import { UserContext } from '../userContext';
-import { useRouter } from 'next/router';
+
 const ReactSelectStyle = (baseStyles, state) => ({
   ...baseStyles,
   borderColor: state.isFocused ? '#3182ce' : '#a0aec0',
@@ -164,8 +165,9 @@ export default function TowingCarsRequestForm({
             vin: intl.formatMessage({ id: 'form.car.exist' }),
           }));
         }
-      }).catch((error) => {
-        console.error("Error request on existing car:", error);
+      })
+      .catch((error) => {
+        console.error('Error request on existing car:', error);
       });
   }, [carData.lotnumber, carData.vin]);
 
@@ -342,22 +344,22 @@ export default function TowingCarsRequestForm({
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-   
+
     // refresh next-auth session
     // alert("profile.customer_id: "+profile.customer_id)
     await fetch('/api/auth/session');
-    
-    const checkSessionResponse = await fetch('/api/customer/checksession_exp');
-  const { expired } = await checkSessionResponse.json();
 
-  if (expired) {
-    // Redirect to the login page if the session is expired
-    router.push('/login');
-    return;
-  }
+    const checkSessionResponse = await fetch('/api/customer/checksession_exp');
+    const { expired } = await checkSessionResponse.json();
+
+    if (expired) {
+      // Redirect to the login page if the session is expired
+      router.push('/login');
+      return;
+    }
     // alert("Profile:", profile); // Add this line
     // console.log("Profile Customer ID:", profile?.customer_id); // Add this line
-      // Enhanced validation: Check if profile or customer_id is null/undefined or 0
+    // Enhanced validation: Check if profile or customer_id is null/undefined or 0
     if (!profile || !profile.customer_id || profile.customer_id == 0) {
       setFormSubmitModal({
         status: true,
@@ -384,9 +386,9 @@ export default function TowingCarsRequestForm({
         formData.append('photoFile', photoFile);
       }
       // Log formData entries
-    for (let pair of formData.entries()) {
-      console.log(pair[0]+ ', ' + pair[1]);
-    }
+      for (const pair of formData.entries()) {
+        console.log(`${pair[0]}, ${pair[1]}`);
+      }
       try {
         const response = await fetch('/api/customer/cars/warehouseCars/', {
           method: 'POST',
@@ -411,7 +413,6 @@ export default function TowingCarsRequestForm({
           type: 'success',
           message: 'Saved successfully.',
         });
-
       } catch (error) {
         console.error(error);
         setFormSubmitModal({
@@ -419,7 +420,6 @@ export default function TowingCarsRequestForm({
           type: 'error',
           message: `Unable to save. Something went wrong: ${error.message}`,
         });
-
       } finally {
         setSubmitStarted(false);
         document.documentElement.style.overflow = 'auto';
@@ -1070,7 +1070,60 @@ export default function TowingCarsRequestForm({
                     {getValidationMessage('gate_pass_pin')}
                   </div>
                 </div>
-              
+                <div className="my-4 gap-2 sm:flex">
+                  <div className="w-full">
+                    <label className="text-teal-blue block text-lg rtl:text-right">
+                      <FormattedMessage id="form.car_photo" />
+                      <span
+                        className={classNames(
+                          carData.id ? 'hidden' : '',
+                          'mx-1 text-lg text-red-500'
+                        )}
+                      >
+                        *
+                      </span>
+                    </label>
+                    <input
+                      className="w-full rounded-md border px-1 text-lg text-gray-700"
+                      type="file"
+                      name="car_photo"
+                      accept="image/png, image/gif, image/jpeg"
+                      required={
+                        !carData.id &&
+                        carData.id !== '' &&
+                        wizardStepIndex.current >= 1 // in which field is shown
+                      }
+                      onChange={handlePhotoFileChange}
+                    />
+                    {getValidationMessage('car_photo')}
+                  </div>
+                  <div className="w-full">
+                    <label className="text-teal-blue block text-lg rtl:text-right">
+                      <FormattedMessage id="form.invoice" />
+                      <span
+                        className={classNames(
+                          carData.id ? 'hidden' : '',
+                          'mx-1 text-lg text-red-500'
+                        )}
+                      >
+                        *
+                      </span>
+                    </label>
+                    <input
+                      className="w-full rounded-md border px-1 text-lg text-gray-700"
+                      type="file"
+                      name="invoice"
+                      accept="application/pdf"
+                      required={
+                        !carData.id &&
+                        carData.id !== '' &&
+                        wizardStepIndex.current >= 1 // in which field is shown
+                      }
+                      onChange={handleInvoiceFileChange}
+                    />
+                    {getValidationMessage('invoice')}
+                  </div>
+                </div>
               </div>
             </FormWizard.TabContent>
           </FormWizard>
